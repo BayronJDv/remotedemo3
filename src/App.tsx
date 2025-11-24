5a6fb37a1a7ef2764be25b4b2af5792b34e11534
Bayron Jojoa <bayron.jojoa.dv@gmail.com>
2025-11-24 09:45:19 -0500

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css';
+import Lateral from './components/Lateral';
+
+const App = () => {
+  return (
+    <div className="content">
+      <Lateral />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/Lateral.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Lateral.css';
+
+const Lateral: React.FC = () => { 
+  return (
+    <div className="lateralparent">
+      <h1>lateral panel </h1>
+    </div>
+  );
+};
+
+export default Lateral;
